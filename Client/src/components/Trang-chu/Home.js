a04f6e526af87b42c82385a47566d9c5399db570
ThaiQuocTam <thaiquoctam123@gmail.com>
2022-09-21 21:51:02 +0700

    commit update header

--- Client/src/components/Trang-chu/Home.js
+++ Client/src/components/Trang-chu/Home.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
+    const image = [
+        "https://source.unsplash.com/user/erondu/3000x900",
+        "https://source.unsplash.com/collection/190727/3000x900",
+        "https://source.unsplash.com/collection/190728/3000x900"
+    ]
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
     return (
         <>
-            <h1>Trang chủ</h1>
+
+
         </>
     )
 }
--- Client/src/layout/Header.js
+++ Client/src/layout/Header.js
@@ -4,7 +4,7 @@ import image from '../Assets/images/picwish.jpg'
 const Header = () => {
     return (
         <>
-            <header className='h-200 p-5 bg-green-100 pl-30'>
+            <header className='h-200 p-5 pl-30'>
                 <div className=''>
                     <div className='float-left h-40 w-40 relative position-top mr-10'>
                         <a href='#' className='block w-full h-full'>
@@ -12,33 +12,16 @@ const Header = () => {
                         </a>
                     </div>
                     <ul className=''>
-                        {/* <li className='inline-block mr-10 '>
-                            <a href='#' className='block w-5 h-5'>
-                                <img className='w-full h-full' src={image} />
-                            </a>
-                        </li> */}
-                        <li className='inline-block mr-16'>
+                        <li className='inline-block mr-24'>
                             <div>
-                                <input className='border focus:outline-none border-green-700 hover:border-green-900 focus:border placeholder:text-3.5 placeholder:text-slate-500 focus:border-green-900 rounded-5 h-10 w-90 mr-3 pl-5' type={'text'} placeholder='Nhập sản phẩm cần tìm...' />
+                                <input className='border focus:outline-none border-green-700 hover:border-green-900 focus:border placeholder:text-3.5 placeholder:text-slate-500 focus:border-green-900 rounded-5 h-10 w-120 mr-3 pl-5' type={'text'} placeholder='Nhập sản phẩm cần tìm...' />
                                 <a href='#'><i className="bi bi-search text-7 cursor-pointer leading-6-em block float-right"></i></a>
                             </div>
-                        </li>
-                        <li className='inline-block mr-14 hover:opacity-50'>
-                            <a href='#'>
-                                <span className='font-semibold'>Điện thoại</span>
-                                <i className="bi bi-phone-fill text-5 text-green-700"></i>
-                            </a>
-                        </li>
-                        <li className='inline-block mr-14 hover:opacity-50'>
-                            <a href='#'>
-                                <span className='font-semibold mr-1'>Máy tính bảng</span>
-                                <i className="bi bi-tablet-landscape-fill text-5 text-green-700"></i>
-                            </a>
                         </li>
                         <li className='inline-block mr-14 text-6 hover:opacity-50'>
                             <a className='inline-block' href='#'>
                                 <i className="bi bi-cart2 inline-block text-green-900 text-7 pr-1"></i>
-                                <span className='w-8 text-center text-white text-4 p-1 bg-orange-500 rounded-2 block float-right'>
+                                <span className='w-7 h-7 text-center text-white text-3.5 p-1 pb-1 bg-orange-500 rounded-2 block float-right'>
                                     0
                                 </span>
                             </a>
@@ -46,33 +29,33 @@ const Header = () => {
                         <li className='inline-block mr-14 hover:opacity-50'>
                             <a href='#'>
                                 <i className="bi bi-clock-history text-6 pr-2 text-green-700"></i>
-                                <span className='inline-block pb-2 font-semibold'>Tra cứu đơn hàng</span>
+                                <span className='inline-block pb-2 text-3.5 font-semibold'>Tra cứu đơn hàng</span>
                             </a>
                         </li>
                         <li className='inline-block mr-10 hover:opacity-50'>
                             <div className='cursor-pointer'>
                                 <i className="bi bi-person-fill text-6 pr-2 text-green-700"></i>
-                                <span className='font-semibold'>Đăng nhập</span>
+                                <span className='font-semibold text-3.5'>Đăng nhập</span>
                             </div>
                         </li>
                     </ul>
                 </div >
-                {/* <div className='text-center m-5'>
+                <div className='mt-5 mr-32 ml-4 p-1 bg-green-200 rounded-2'>
                     <ul className=''>
-                        <li className='inline-block'>
+                        <li className='inline-block mr-14 hover:opacity-50'>
                             <a href='#'>
-                                <span>Điện thoại</span>
-                                <i className="bi bi-phone-fill"></i>
+                                <span className='font-medium mr-1'>Điện thoại</span>
+                                <i className="bi bi-phone-fill text-5 text-green-700"></i>
                             </a>
                         </li>
-                        <li className='inline-block'>
+                        <li className='inline-block mr-14 hover:opacity-50'>
                             <a href='#'>
-                                <span>Máy tính bảng</span>
-                                <i className="bi bi-tablet-landscape-fill"></i>
+                                <span className='font-medium mr-1'>Máy tính bảng</span>
+                                <i className="bi bi-tablet-landscape-fill text-5 text-green-700"></i>
                             </a>
                         </li>
                     </ul >
-                </div > */}
+                </div >
             </header >
         </>
 
