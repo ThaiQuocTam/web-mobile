605bf46e7d7adaa85c9a1163a515b5139f9db549
ThaiQuocTam <thaiquoctam123@gmail.com>
2022-09-30 19:37:09 +0700

    Update BackHome.js

--- Client/src/components/Trang-chu/BackHome.js
+++ Client/src/components/Trang-chu/BackHome.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 const BackHome = () => {
     return (
         <>
-            <Link className="relative right-44 leading-9 h-9 mb-5 block text-green-600 hover:text-green-800" to="/">
-                <i className="bi bi-arrow-left-circle icon text-8  mr-2 " />
-                <strong className='text-4  '>Quay lại</strong>
-            </Link  >
+            <div className='overflow-auto w-full pl-32'>
+                <Link className="relative float-left leading-9 h-9 mb-5 block text-green-900 hover:text-green-600" to="/">
+                    <i className="bi bi-arrow-left-circle icon text-8  mr-2 " />
+                    <strong className='text-4  '>Quay lại</strong>
+                </Link  >
+            </div>
         </>
     )
 }
