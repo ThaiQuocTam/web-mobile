7b5e900d6ffc06f4dca68e21f95d65dddc9f5f45
ThaiQuocTam <thaiquoctam123@gmail.com>
2022-10-06 14:38:35 +0700

    cap nhat UI OrderDetail

--- Client/src/components/Thong-tin-don-hang/OrderDetail.js
+++ Client/src/components/Thong-tin-don-hang/OrderDetail.js
@@ -1,6 +1,5 @@
 import BackHome from 'components/Trang-chu/BackHome'
 import React from 'react'
-import './OrderDetail.css';
 
 const OrderDetail = () => {
     return (
@@ -14,37 +13,37 @@ const OrderDetail = () => {
                         <i class="bi bi-check2-circle text-16 cursor-pointer leading-4-em block text-green-900"></i>
                     </div>
                     <div className="mt-4">
-                        <h2 className='text-center font-medium text-black font-sans'>THÔNG TIN ĐƠN HÀNG </h2>
+                        <span className='text-center block text-7 font-semibold text-black'>THÔNG TIN ĐƠN HÀNG </span>
                     </div>
                     <div className="mt-4">
-                        <label className='labelone font-bold mb-3 '>Thông tin người đặt hàng</label>
-                        <div class="border border-gray-400 w-full rounded-3 pl-5" >
+                        <label className='font-bold mb-3 text-5'>Thông tin người đặt hàng</label>
+                        <div class="border border-gray-400 py-2 w-full rounded-3 pl-5" >
                             <div class="divide-y divide-dashed ">
-                                <div className='grid grid-cols-3 divide-x' >
-                                    <label className='labelone'>Họ và tên: </label>
-                                    <div className=''>
-                                        <label className='labelone'> Nguyễn Trung Nghĩa</label>
+                                <div className='grid grid-cols-3 divide-x mb-1' >
+                                    <label className='text-4'>Họ và tên: </label>
+                                    <div className='pl-2'>
+                                        <label className='text-3.5'> Nguyễn Trung Nghĩa</label>
                                     </div>
                                 </div>
 
-                                <div className='grid grid-cols-3 divide-x' >
-                                    <label className='labelone'>Số điện thoại: </label>
-                                    <div className='labelone'>
-                                        <label className=''> 0388339515</label>
+                                <div className='grid grid-cols-3 divide-x mb-1' >
+                                    <label className='text-4'>Số điện thoại: </label>
+                                    <div className='pl-2'>
+                                        <label className='text-3.5'> 0388339515</label>
                                     </div>
                                 </div>
 
-                                <div className='grid grid-cols-3 divide-x' >
-                                    <label className='labelone'>Email: </label>
-                                    <div className=''>
-                                        <label className='labelone'> NN@gmail.com</label>
+                                <div className='grid grid-cols-3 divide-x mb-1' >
+                                    <label className='text-4'>Email: </label>
+                                    <div className='pl-2'>
+                                        <label className='text-3.5'> NN@gmail.com</label>
                                     </div>
                                 </div>
 
-                                <div className='grid grid-cols-3 divide-x' >
-                                    <label className='labelone'>Địa chỉ: </label>
-                                    <div className=''>
-                                        <label className='labelone'> Gia Lai</label>
+                                <div className='grid grid-cols-3 divide-x mb-1' >
+                                    <label className='text-4'>Địa chỉ: </label>
+                                    <div className='pl-2'>
+                                        <label className='text-3.5'> Gia Lai</label>
                                     </div>
                                 </div>
                             </div>
@@ -52,32 +51,32 @@ const OrderDetail = () => {
                     </div>
 
                     <div className='mt-10'>
-                        <label className='labelone font-bold mb-3'>Danh sách sản phẩm</label>
-                        <table class=" border-separate border-spacing-2 border-0 border-green-900 w-full ">
-                            <thead className='bg-green-700 text-white'>
-                                <tr>
-                                    <th className='labelone border border-slate-900 text-center'>STT</th>
-                                    <th className='labelone border border-slate-900 text-center'>Tên sản phẩm</th>
-                                    <th className='labelone border border-slate-900 text-center'>Số lượng</th>
-                                    <th className='labelone border border-slate-900 text-center'>Giá</th>
-                                    <th className='labelone border border-slate-900 text-center'>Tổng (Số lượng x giá)</th>
+                        <label className='text-5 font-bold mb-3'>Danh sách sản phẩm</label>
+                        <table class="table-auto w-full rounded-3 border border-slate-200 border-collapse">
+                            <thead className='bg-green-700 rounded-2 text-white leading-normal'>
+                                <tr className=''>
+                                    <th className='text-3.5 py-1 text-center'>STT</th>
+                                    <th className='text-3.5  text-center'>Tên sản phẩm</th>
+                                    <th className='text-3.5  text-center'>Số lượng</th>
+                                    <th className='text-3.5  text-center'>Giá</th>
+                                    <th className='text-3.5  text-center'>Tổng (Số lượng x giá)</th>
                                 </tr>
                             </thead>
-                            <tbody>
-                                <tr>
-                                    <td className='labelone border border-slate-900 text-center'>1</td>
-                                    <td className='labelone border border-slate-900 text-center'>Điện thoại Nokia</td>
-                                    <td className='labelone border border-slate-900 text-center'>2</td>
-                                    <td className='labelone border border-slate-900 text-center'>20000đ</td>
-                                    <td className='labelone border border-slate-900 text-center'>50000000đ</td>
+                            <tbody className=''>
+                                <tr className='bg-slate-50 hover:bg-slate-100'>
+                                    <td className='text-3.5 p-2 text-center'>1</td>
+                                    <td className='text-3.5 p-2 text-center'>Điện thoại Nokia</td>
+                                    <td className='text-3.5 p-2 text-center'>2</td>
+                                    <td className='text-3.5 p-2 text-center'>20000đ</td>
+                                    <td className='text-3.5 p-2 text-center'>50000000đ</td>
 
                                 </tr>
-                                <tr>
-                                    <td className='labelone border border-slate-900 text-center'>2</td>
-                                    <td className='labelone border border-slate-900 text-center'>Điện thoại samsung</td>
-                                    <td className='labelone border border-slate-900 text-center'>10</td>
-                                    <td className='labelone border border-slate-900 text-center'>25000đ</td>
-                                    <td className='labelone border border-slate-900 text-center'>25000000đ</td>
+                                <tr className='bg-slate-50 hover:bg-slate-100'>
+                                    <td className='text-3.5 p-2 text-center'>2</td>
+                                    <td className='text-3.5 p-2  text-center'>Điện thoại samsung</td>
+                                    <td className='text-3.5 p-2  text-center'>10</td>
+                                    <td className='text-3.5 p-2  text-center'>25000đ</td>
+                                    <td className='text-3.5 p-2  text-center'>25000000đ</td>
                                 </tr>
 
                             </tbody>
