fbeb43f34c946a314529f6ae45d4fc84ff56045a
Nguyen Trung Nghia <nghianguyen00108@gmail.com>
2022-10-26 13:17:53 +0700

    Update QlThanhVien.js

--- Admin/src/components/Ql-thanh-viên/QlThanhVien.js
+++ Admin/src/components/Ql-thanh-viên/QlThanhVien.js
@@ -2,7 +2,56 @@ import React from 'react'
 
 const QlThanhVien = () => {
     return (
-        <div>QlThanhVien</div>
+        <>
+        <div className='font-bold text-3xl mb-3'>Danh sách hóa đơn</div>
+        <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            <table className="min-w-full">
+                                <thead className="bg-gray-400 border-b">
+                                    <tr>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            STT
+                                        </th>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Họ và tên
+                                        </th>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Địa chỉ nhận hàng
+                                        </th>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Tổng thanh toán
+                                        </th>
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Trạng thái
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Nguyễn Bá Tín
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            Quy nhơn
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            2.300.000đ
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <a href="#" className="px-4 py-1 text-sm text-white bg-blue-400 rounded mr-4">Xem chi tiết</a>
+                                            <a href="#" className="px-4 py-1 text-sm text-white bg-blue-400 rounded">Xóa</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </>
     )
 }
 
