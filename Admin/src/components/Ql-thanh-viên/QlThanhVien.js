01273f319b701f5826039cf85cbc0298f442cb81
Nguyen Trung Nghia <nghianguyen00108@gmail.com>
2022-10-26 16:49:58 +0700

    update modalThemTV 26/10/2022

--- Admin/src/components/Ql-thanh-viên/QlThanhVien.js
+++ Admin/src/components/Ql-thanh-viên/QlThanhVien.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ModalThemTV from './ModalThemTV'
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../../redux/actions/actions'
+import { Link } from 'react-router-dom';
 
 const QlThanhVien = () => {
+    const [showModalAddMember, setShowModalAddMember] = useState(false)
+
+    const dispatch = useDispatch()
+
+
+
+    const hideModalAddMember = () => {
+        setShowModalAddMember(false)
+    }
     return (
         <>
             <div className='font-bold text-3xl mb-3'>Danh sách hóa đơn</div>
@@ -9,8 +22,8 @@ const QlThanhVien = () => {
                     <input className='border focus:outline-none border-green-700 text-3.5 hover:border-green-900 focus:border placeholder:text-3 placeholder:text-slate-500 focus:border-green-900 rounded-5 h-10 w-96 mr-3 px-5' type={'text'} placeholder='Nhập sản phẩm cần tìm...' />
                     <a className='inline-block h-10 leading-10' href='#'><i className="bi bi-search text-gray-600 h-10 inline-block hover:text-black text-5 cursor-pointer leading-10"></i></a>
                 </div>
-                <div className="leading-9 h-9 mb-5 inline-block ml-30rem" >
-                    <strong className='text-3.5 text-black border-gray-500 border-2 px-3 pr-5 py-3 ml-2 rounded-2 cursor-pointer hover:bg-slate-600 hover:text-white'> + &ensp; Thêm Sản Phẩm</strong>
+                <div className="leading-9 h-9 mb-5 inline-block ml-30rem" onClick={() => setShowModalAddMember(true)}>
+                    <strong className='text-3.5 text-black border-gray-500 border-2 px-3 pr-5 py-3 ml-2 rounded-2 cursor-pointer hover:bg-slate-600 hover:text-white'> + &ensp; Thêm Thành Viên</strong>
                 </div>
             </div>
             <div className="flex flex-col">
@@ -73,6 +86,12 @@ const QlThanhVien = () => {
                     </div>
                 </div>
             </div>
+            {showModalAddMember &&
+
+                <div className='fixed flex z-sticky items-center bg-slate-250 justify-center left-0 top-0 right-0 bottom-0'>
+                    <ModalThemTV isClose={hideModalAddMember} />
+                </div>
+            }
         </>
     )
 }
